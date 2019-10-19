1f8e4418012ed590cfd01c813e51f059185432c9
cristal28 <cramos0295@eagle.fgcu.edu>
2019-10-19 18:04:51 -0400

    Initial commit

---
+++ Main.py
@@ -0,0 +1,41 @@
+def main():
+    print("Hello! Welcome to the main menu.")
+    userContinue = True
+    while userContinue:
+        print("Enter a choice:")
+        print("1. ")
+        print("2. ")
+        print("3. ")
+        print("4. ")
+        print("5. ")
+        print("6. ")
+        print("7. ")
+        print("8. ")
+        print("9. ")
+        print("10. ")
+        print("11. Exit")
+        selection = int(input())
+        if selection == 1 :
+
+        elif selection == 2:
+
+        elif selection == 3:
+
+        elif selection == 4:
+
+        elif selection == 5:
+
+        elif selection == 6:
+
+        elif selection == 7:
+
+        elif selection == 8:
+
+        elif selection == 9:
+
+        elif selection == 10:
+
+        else:
+            print("Error: Invalid Selection")
+
+main()
