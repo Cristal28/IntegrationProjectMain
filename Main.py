73eca2e6e5db6c7a8034508937f1726cd5a4f34b
cristal28 <cramos0295@eagle.fgcu.edu>
2019-11-22 10:40:11 -0500

    Reformatting

--- Main.py
+++ Main.py
@@ -12,14 +12,15 @@ def main():
     """
     print("Welcome to 'Python Basics' tutorial!")
     print(
-        "Read the main menu careful to select which tutorial you'd like to see.")
+        "Read the main menu careful to select which tutorial you'd like to "
+        "see.")
     print("Main Menu")
     user_continue = True
     while user_continue:
         print(
             "Enter the number corresponding to the tutorial you want to see:")
-        print("1. ")
-        print("2. ")
+        print("1. How to use Literal Strings ")
+        print("2. How to use ")
         print("3. ")
         print("4. ")
         print("5. ")
@@ -31,18 +32,29 @@ def main():
         print("11. Exit")
         number = int(input())
         if number == 1:
+            print("You have chosen the following tutorial '___' ")
         # Fix indentation problem here
         elif number == 2:
+            print("You have chosen the following tutorial '___' ")
         elif number == 3:
+            print("You have chosen the following tutorial '___' ")
         elif number == 4:
+            print("You have chosen the following tutorial '___' ")
         elif number == 5:
+            print("You have chosen the following tutorial '___' ")
         elif number == 6:
+            print("You have chosen the following tutorial '___' ")
         elif number == 7:
+            print("You have chosen the following tutorial '___' ")
         elif number == 8:
+            print("You have chosen the following tutorial '___' ")
         elif number == 9:
+            print("You have chosen the following tutorial '___' ")
         elif number == 10:
+            print("You have chosen the following tutorial '___' ")
         else:
             print("Error: Invalid Selection")
+            print("Please enter a choice from the main menu.")
 
 
 main()
